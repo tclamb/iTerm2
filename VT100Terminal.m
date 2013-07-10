tclamb/iTerm2

commit cf2fbc2f2ac0bd5c0ee5e020e4ca89130f9d7cc7

logic and style fixes

VT100Terminal.m
@@ -3480,9 +3480,13 @@ static VT100TCC decode_string(unsigned char *datap,
     // all attributes off
     bold = italic = under = blink = reversed = NO;
     FG_COLORCODE = ALTSEM_FG_DEFAULT;
+    FG_GREEN = 0;
+    FG_BLUE = 0;
     FG_IS24BIT = NO;
     alternateForegroundSemantics = YES;
     BG_COLORCODE = ALTSEM_BG_DEFAULT;
+    BG_GREEN = 0;
+    BG_BLUE = 0;
     BG_IS24BIT = NO;
     alternateBackgroundSemantics = YES;
 }

ScreenChar.h
@@ -176,15 +176,15 @@ static inline void CopyBackgroundColor(screen_char_t* to, const screen_char_t fr
 static inline BOOL BackgroundColorsEqual(const screen_char_t a,
                                          const screen_char_t b)
 {
-    if (!(a.bgIs24bit && b.bgIs24bit)) {
+    if (!a.bgIs24bit && !b.bgIs24bit) {
         return a.backgroundColor == b.backgroundColor &&
         a.alternateBackgroundSemantics == b.alternateBackgroundSemantics;
-    } else if (a.bgIs24bit == !b.bgIs24bit) {
-        return false;
+    } else if (a.bgIs24bit != b.bgIs24bit) {
+        return NO;
     } else {
         return a.backgroundColor == b.backgroundColor &&
         a.bgGreen == b.bgGreen &&
-        a.bgBlue  == b.bgBlue  &&
+        a.bgBlue == b.bgBlue  &&
         a.alternateBackgroundSemantics == b.alternateBackgroundSemantics;
     }
 }
@@ -193,19 +193,19 @@ static inline BOOL BackgroundColorsEqual(const screen_char_t a,
 static inline BOOL ForegroundColorsEqual(const screen_char_t a,
                                          const screen_char_t b)
 {
-    if (!(a.fgIs24bit && b.fgIs24bit)) {
+    if (!a.fgIs24bit && !b.fgIs24bit) {
         return a.foregroundColor == b.foregroundColor &&
         a.alternateForegroundSemantics == b.alternateForegroundSemantics &&
         a.bold == b.bold &&
         a.italic == b.italic &&
         a.blink == b.blink &&
         a.underline == b.underline;
-    } else if (a.fgIs24bit == !b.fgIs24bit) {
-        return false;
+    } else if (a.fgIs24bit != b.fgIs24bit) {
+        return NO;
     } else {
-        return a.foregroundColor   == b.foregroundColor &&
+        return a.foregroundColor == b.foregroundColor &&
         a.fgGreen == b.fgGreen &&
-        a.fgBlue  == b.fgBlue  &&
+        a.fgBlue == b.fgBlue  &&
         a.alternateForegroundSemantics == b.alternateForegroundSemantics;
     }
 }

VT100Screen.m
@@ -147,22 +147,22 @@ void StringToScreenChars(NSString *s,
             buf[j].complexChar = NO;
 
             buf[j].foregroundColor = fg.foregroundColor;
-            if ((buf[j].fgIs24bit = fg.fgIs24bit)) {
-                 buf[j].fgGreen   = fg.fgGreen;
-                 buf[j].fgBlue    = fg.fgBlue;
-            }
+            buf[j].fgGreen = fg.fgGreen;
+            buf[j].fgBlue = fg.fgBlue;
+
+            buf[j].backgroundColor = bg.backgroundColor;
+            buf[j].bgGreen = bg.bgGreen;
+            buf[j].bgBlue = bg.bgBlue;
+
+            buf[j].fgIs24bit = fg.fgIs24bit;
+            buf[j].bgIs24bit = bg.bgIs24bit;
             buf[j].alternateForegroundSemantics = fg.alternateForegroundSemantics;
+            buf[j].alternateBackgroundSemantics = bg.alternateBackgroundSemantics;
+
             buf[j].bold = fg.bold;
             buf[j].italic = fg.italic;
             buf[j].blink = fg.blink;
             buf[j].underline = fg.underline;
-
-            buf[j].backgroundColor = bg.backgroundColor;
-            if ((buf[j].bgIs24bit = bg.bgIs24bit)) {
-                 buf[j].bgGreen   = bg.bgGreen;
-                 buf[j].bgBlue    = bg.bgBlue;
-            }
-            buf[j].alternateBackgroundSemantics = bg.alternateBackgroundSemantics;
 
             buf[j].unused = 0;
             lastInitializedChar = j;
@@ -182,22 +182,24 @@ void StringToScreenChars(NSString *s,
             buf[j].complexChar = NO;
 
             buf[j].foregroundColor = fg.foregroundColor;
-            if ((buf[j].fgIs24bit = fg.fgIs24bit)) {
-                 buf[j].fgGreen   = fg.fgGreen;
-                 buf[j].fgBlue    = fg.fgBlue;
-            }
+            buf[j].fgGreen = fg.fgGreen;
+            buf[j].fgBlue = fg.fgBlue;
+
+            buf[j].backgroundColor = bg.backgroundColor;
+            buf[j].bgGreen = bg.fgGreen;
+            buf[j].bgBlue = bg.fgBlue;
+
+            buf[j].fgIs24bit = fg.fgIs24bit;
+            buf[j].bgIs24bit = bg.fgIs24bit;
             buf[j].alternateForegroundSemantics = fg.alternateForegroundSemantics;
+            buf[j].alternateBackgroundSemantics = bg.alternateBackgroundSemantics;
+
             buf[j].bold = fg.bold;
             buf[j].italic = fg.italic;
             buf[j].blink = fg.blink;
             buf[j].underline = fg.underline;
 
-            buf[j].backgroundColor = bg.backgroundColor;
-            if ((buf[j].bgIs24bit = bg.fgIs24bit)) {
-                 buf[j].bgGreen   = bg.fgGreen;
-                 buf[j].bgBlue    = bg.fgBlue;
-            }
-            buf[j].alternateBackgroundSemantics = bg.alternateBackgroundSemantics;
+            buf[j].unused = 0;
         } else if (sc[i] == 0xfeff ||  // zero width no-break space
                    sc[i] == 0x200b ||  // zero width space
                    sc[i] == 0x200c ||  // zero width non-joiner
@@ -225,22 +227,24 @@ void StringToScreenChars(NSString *s,
                         buf[j].complexChar = NO;
 
                         buf[j].foregroundColor = fg.foregroundColor;
-                        if ((buf[j].fgIs24bit = fg.fgIs24bit)) {
-                             buf[j].fgGreen   = fg.fgGreen;
-                             buf[j].fgBlue    = fg.fgBlue;
-                        }
+                        buf[j].fgGreen = fg.fgGreen;
+                        buf[j].fgBlue = fg.fgBlue;
+
+                        buf[j].backgroundColor = bg.backgroundColor;
+                        buf[j].bgGreen = bg.fgGreen;
+                        buf[j].bgBlue = bg.fgBlue;
+
+                        buf[j].fgIs24bit = fg.fgIs24bit;
+                        buf[j].bgIs24bit = bg.fgIs24bit;
                         buf[j].alternateForegroundSemantics = fg.alternateForegroundSemantics;
+                        buf[j].alternateBackgroundSemantics = bg.alternateBackgroundSemantics;
+
                         buf[j].bold = fg.bold;
                         buf[j].italic = fg.italic;
                         buf[j].blink = fg.blink;
                         buf[j].underline = fg.underline;
 
-                        buf[j].backgroundColor = bg.backgroundColor;
-                        if ((buf[j].bgIs24bit = bg.fgIs24bit)) {
-                             buf[j].bgGreen   = bg.fgGreen;
-                             buf[j].bgBlue    = bg.fgBlue;
-                        }
-                        buf[j].alternateBackgroundSemantics = bg.alternateBackgroundSemantics;
+                        buf[j].unused = 0;
                     }
                 }
             }
@@ -840,12 +844,12 @@ static char* FormatCont(int c)
         assert(theLine);
         if (theLine) {
             if (fgColor) {
-                theLine[x].alternateForegroundSemantics = NO;
                 theLine[x].foregroundColor = fgColorCode;
+                theLine[x].alternateForegroundSemantics = NO;
             }
             if (bgColor) {
-                theLine[x].alternateBackgroundSemantics = NO;
                 theLine[x].backgroundColor = bgColorCode;
+                theLine[x].alternateBackgroundSemantics = NO;
             }
         }
         ++x;
@@ -994,7 +998,7 @@ static char* FormatCont(int c)
     // loop doesn't call dropExcessLinesWithWidth.
     int next_line_length;
     if (numLines > 0) {
-        next_line_length  = [self _getLineLength:[self getLineAtScreenIndex: 0]];
+        next_line_length = [self _getLineLength:[self getLineAtScreenIndex: 0]];
     }
     for (i = 0; i < numLines; ++i) {
         screen_char_t* line = [self getLineAtScreenIndex: i];
@@ -3917,7 +3921,7 @@ void DumpBuf(screen_char_t* p, int n) {
 
     }
     if (n + cursorY > SCROLL_BOTTOM) {
-        n  = SCROLL_BOTTOM - cursorY + 1;
+        n = SCROLL_BOTTOM - cursorY + 1;
     }
 
     // clear the n lines

commit 0dddad1c85d7b3c2e8afa8aea7dfa6f9e70aed5b

tracked down bug introduced in cf2fbc2f2ac0bd5c0ee5e020e4ca89130f9d7cc7

VT100Terminal.m
@@ -3464,11 +3464,11 @@ static VT100TCC decode_string(unsigned char *datap,
     FG_COLORCODE = ALTSEM_FG_DEFAULT;
     FG_GREEN = 0;
     FG_BLUE = 0;
-    FG_COLORMODE = ColorModeNormal;
+    FG_COLORMODE = ColorModeAlternate;
     BG_COLORCODE = ALTSEM_BG_DEFAULT;
     BG_GREEN = 0;
     BG_BLUE = 0;
-    FG_COLORMODE = ColorModeNormal;
+    FG_COLORMODE = ColorModeAlternate;
 }
 
 - (void)_setCharAttr:(VT100TCC)token